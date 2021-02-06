be139c725705a44861dbb139a156f0bb77a24ed7
Tonny <tonny.litao@gmail.com>
2021-02-06 22:15:48 +1300

    Update BasicsBasic Types_26_1e576d25ed19ebf92fbc9a96305153a9.swift

--- language-code/doc-kotlin/BasicsBasic Types_26_1e576d25ed19ebf92fbc9a96305153a9.swift
+++ language-code/doc-kotlin/BasicsBasic Types_26_1e576d25ed19ebf92fbc9a96305153a9.swift
@@ -1,3 +1,3 @@
 // Creates an Array<String> with values ["0", "1", "4", "9", "16"]
-val asc = Array(5) { i -> (i * i).toString() }
-asc.forEach { println(it) }
+let asc = (0..<5).map { String($0 * $0) }
+asc.forEach { print($0) }
