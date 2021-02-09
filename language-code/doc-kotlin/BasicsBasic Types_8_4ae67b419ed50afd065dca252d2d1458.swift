466b1d2e08499a71c220a1acf3d43e40151d7c26
Tonny <tonny.litao@gmail.com>
2021-02-09 21:34:58 +1300

    add swift for Basic Types

--- language-code/doc-kotlin/BasicsBasic Types_8_4ae67b419ed50afd065dca252d2d1458.swift
+++ language-code/doc-kotlin/BasicsBasic Types_8_4ae67b419ed50afd065dca252d2d1458.swift
@@ -1,10 +1,10 @@
 let a: Int = 100
 let boxedA: Int? = a
 let anotherBoxedA: Int? = a
-​
+
 let b: Int = 10000
 let boxedB: Int? = b
 let anotherBoxedB: Int? = b
-​
-print(boxedA === anotherBoxedA) // true
-print(boxedB === anotherBoxedB) // false
+
+print(boxedA == anotherBoxedA) // true
+print(boxedB == anotherBoxedB) // true
